528489c915620d0542249cd538107c3285532d9c
Stephen Thomas <80729552+stephenthomas04@users.noreply.github.com>
2022-10-05 11:02:17 -0500

    10/5/22
    Added firebase and work on Day 2 and 3 on home

--- app/src/main/java/com/example/firebasememorybox/SelectActionActivity.java
+++ app/src/main/java/com/example/firebasememorybox/SelectActionActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SignInActivity extends AppCompatActivity {
+public class SelectActionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        setContentView(R.layout.activity_select_action);
     }
 }
